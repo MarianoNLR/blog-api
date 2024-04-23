8f403186d4130f5371957aef570dca6604e11b5f
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-04-22 21:16:03 -0300

    express app

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import 'dotenv/config'
 import express from 'express'
+import './mongo.js'
+import bodyParser from 'body-parser'
+import { createUserRouter } from './routes/user.js'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const PORT = process.env.PORT ?? 3000
+
+app.use('/users', createUserRouter())
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`)
+})
