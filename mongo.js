f985a4e998dbed520fda906f8279c9770fb991e6
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-04-22 21:14:58 -0300

    mongodb connection

---
+++ mongo.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const { MONGO_DB_URI } = process.env
+
+const connectionString = MONGO_DB_URI
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Database connected')
+  }).catch(err => {
+    console.error(err)
+  })
+
+process.on('uncaughtException', error => {
+  console.error(error)
+  mongoose.disconnect()
+})
