d39a870b2fc10d0a2ab130eff48868d953ec01b3
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-04-24 20:47:30 -0300

    added posts router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import './mongo.js'
 import bodyParser from 'body-parser'
 import { createUserRouter } from './routes/user.js'
+import { createBlogRouter } from './routes/post.js'
 
 const app = express()
 app.use(bodyParser.json())
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const PORT = process.env.PORT ?? 3000
 
 app.use('/users', createUserRouter())
+app.use('/posts', createBlogRouter())
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`)
